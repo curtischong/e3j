549351bb7c89c78d7dd6864ad6746ff560fa08b4
Curtis Chong <curtischong5@gmail.com>
2024-11-09 14:29:37 -0800

    properly calculate the irreps for 3d features

--- constants.py
+++ constants.py
@@ -1,2 +1,5 @@
 import jax.numpy as jnp
+
 default_dtype = jnp.float32
+EVEN_PARITY = 1 # a mnemonic to remember this is: "multiplying by 1 doesn't change the sign"
+ODD_PARITY = -1
--- spherical_harmonic.py
+++ spherical_harmonic.py
@@ -2,9 +2,9 @@ import jax.numpy as jnp
 
 import sympy as sp
 
-from irrep import IrrepsArray
+from irrep import Irrep
 from spherical_harmonic_playground import _spherical_harmonics
-from constants import default_dtype
+from constants import ODD_PARITY, default_dtype
 
 # map the point to the specified spherical harmonic. normally, when irreps are passed around,
 # we need to map the coords to EACH irrep
@@ -13,7 +13,7 @@ from constants import default_dtype
 # I am using the spherical harmonics definition from e3x
 
 # This is like scatter sum but for feats?
-def map_3d_feats_to_spherical_harmonics(feats_3d: list[list[float]], normalize: bool=False) -> IrrepsArray:
+def map_3d_feats_to_spherical_harmonics(feats_3d: list[list[float]], normalize: bool=False) -> Irrep:
     coefficients = []
     # jnp.zeros((num_car, num_sph), dtype=np.float64),
     for feat in feats_3d:
@@ -22,12 +22,14 @@ def map_3d_feats_to_spherical_harmonics(feats_3d: list[list[float]], normalize: 
         for m in range(-l, l + 1):
             feats.append(_spherical_harmonics(l, m)(*feat))
         coefficients.append(feats)
-    return jnp.array(coefficients, dtype=default_dtype)
+    arr = jnp.array(coefficients, dtype=default_dtype)
+
+    return Irrep(arr, ODD_PARITY) # 3D features are odd parity by default (cause in real life, if you invert a magnetic field to the opposite direction, you'd want the tensor to switch direction as well)
 
 
 # what is a good way to store the features?
 # I think we should just store the features as a jnp array
-def map_1d_feats_to_spherical_harmonics(feats_1d: list[jnp.ndarray]) -> IrrepsArray:
+def map_1d_feats_to_spherical_harmonics(feats_1d: list[jnp.ndarray]) -> Irrep:
     return jnp.array(feats_1d)
 
 # # returns a function that you can pass x,y,z into to get the spherical harmonic
@@ -35,7 +37,7 @@ def map_1d_feats_to_spherical_harmonics(feats_1d: list[jnp.ndarray]) -> IrrepsAr
 #     # TODO: cache the polynomials?
 #     return _spherical_harmonics(l, m)
 
-def tensor_product(irrep1: IrrepsArray, irrep2: IrrepsArray, output_l: int) -> jnp.ndarray:
+def tensor_product(irrep1: Irrep, irrep2: Irrep, output_l: int) -> jnp.ndarray:
     l1 = irrep1.irreps.l
     l2 = irrep2.irreps.l
 
