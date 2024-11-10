4a6191b3ba2b45d290afd308b6d56cf48552331e
Curtis Chong <curtischong5@gmail.com>
2024-11-09 22:57:06 -0800

    tests to confirm that spherical harmonics is consistent with e3nn

--- spherical_harmonics_test.py
+++ spherical_harmonics_test.py
@@ -25,3 +25,15 @@ if __name__ == "__main__":
     assert_matches_e3x([-2, -2.0, -1.0])
 
     assert jnp.array_equal(map_3d_feats_to_spherical_harmonics_repr([[1,1,1]]).array, map_3d_feats_to_spherical_harmonics_repr([[2,2,2]]).array), "two vectors facing the same direction should have the same representation (despite having diff magnitudes)"
+
+    def assert_matches_e3nn(feat):
+        jfeat = jnp.array(feat)
+        e3x_res = spherical_harmonics(jfeat, 1, cartesian_order=False, normalization='racah')
+        e3j_res = jnp.squeeze(map_3d_feats_to_spherical_harmonics_repr([feat]).array[ODD_PARITY_IDX])
+        assert jnp.allclose(e3x_res, e3j_res), f"e3x={e3x_res}, e3j={e3j_res}"
+    # assert it works with e3nn
+    feat = [1.2, 2.0, -1.0]
+    # print(e3nn.spherical_harmonics("1x0e + 1x1o", np.array(feat), normalize=True))
+    print(e3nn.spherical_harmonics("1x0e + 1x1o", jnp.array(feat), normalize=True, normalization="norm").array)
+    e3j_res = jnp.squeeze(map_3d_feats_to_spherical_harmonics_repr([feat]).array[ODD_PARITY_IDX])
+    print(e3j_res)
--- tensor_product_test.py
+++ tensor_product_test.py
@@ -1,11 +1,19 @@
 from spherical_harmonics import map_3d_feats_to_spherical_harmonics_repr
 from tensor_product import tensor_product_v1
 import jax.numpy as jnp
+from e3nn_jax import tensor_product
 from irrep import Irrep
+import e3nn_jax
 
 if __name__ == "__main__":
+    feat1 = [[1,1,1]]
+    feat2 = [[1,1,2]]
 
-    irrep1 = map_3d_feats_to_spherical_harmonics_repr([[1,1,1]])
-    irrep2 = map_3d_feats_to_spherical_harmonics_repr([[1,1,2]])
+    irrep1 = map_3d_feats_to_spherical_harmonics_repr(feat1)
+    irrep2 = map_3d_feats_to_spherical_harmonics_repr(feat2)
 
     print(tensor_product_v1(irrep1, irrep2, 2))
+
+
+    print(e3nn_jax.spherical_harmonics("1x0e + 1x1o", jnp.array(feat), normalize=True, normalization="norm").array)
+    tensor_product()
