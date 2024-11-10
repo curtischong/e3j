0f67b05a4d6727a95d6524a0cebd3ed3de6e2573
Curtis Chong <curtischong5@gmail.com>
2024-11-09 18:13:13 -0800

    more work on tensor product

--- irrep.py
+++ irrep.py
@@ -23,7 +23,7 @@ from constants import ODD_PARITY
 
 @dataclasses.dataclass(init=False)
 class Irrep():
-    array: Float[Array, "parity_index=2   coefficients_for_every_l_and_m=2*(max_l+1)^2  num_feats"]
+    array: Float[Array, "parity=2   coefficients_for_every_l_and_m=2*(max_l+1)^2   num_feats"]
     # why can't we separate the dimensions for l and m? why are we indexing the coefficients in the coefficients_for_every_l_and_m dimension?
     # it's because each l has different numbers of m. e.g. l=0 has 1 m, l=1 has 3, l=2 has 5, etc.
     # if we used different indices for different l and m, the resulting array would look like a staircase, not a rectangle
@@ -52,12 +52,21 @@ class Irrep():
         start_idx_of_l = l**2 # there are l**2 - 1 coefficients for the lower levels of l. (since l=0 has 1 coefficient, l=1 has 3, l=2 has 5, etc)
         return self.array[(start_idx_of_l) + m, ith_feature]
 
+    # returns true if there is no feature at the given parity and index i
+    def is_feature_zero(self, parity: int, ith_feature: int) -> bool:
+        subset = self.array[parity, ith_feature]
+        return jnp.all(subset == 0)
+
+    EVEN_PARITY_IDX = 0
+    ODD_PARITY_IDX = 1
+    # Note: you only use this for predicting outputs I believe. cause it's kinda sus to just throw out all other coefficients, especially the l=0 coefficient
+    # But also I think this is a bad way of predicitng outputs? I think summing across all coefficients is better. I'm only leaving this in here
+    # to document the fact that we are NOT using cartesian order, and the e3x docs said you can get the vector representation by using these indices
     def get_xyz_vectors(self) -> Float[Array, "num_feats 3"]:
         assert self.array.shape[0][0] >= 4, f"This irrep doesn't have enough coefficients to get the xyz vectors. it only has {self.array.shape[0][0]} coefficients"
-        assert self.parity == ODD_PARITY, "xyz vectors (in the standard sense) should be odd parity. But you're trying to read the xyz vectors of an even parity irrep"
 
         # since we are NOT using cartesian order (see https://e3x.readthedocs.io/stable/pitfalls.html), we need to rearrange the array
-        y = self.array[:,1]
-        z = self.array[:,2]
-        x = self.array[:,3]
+        y = self.array[self.ODD_PARITY_IDX,1,:] # start at index=1 since that is the start of the coefficients of l=1
+        z = self.array[self.ODD_PARITY_IDX,2,:]
+        x = self.array[self.ODD_PARITY_IDX,3,:]
         return jnp.stack([x, y, z], axis=1)
--- tensor_product.py
+++ tensor_product.py
@@ -21,20 +21,24 @@ def tensor_product_v1(irrep1: Irrep, irrep2: Irrep, output_l: int) -> jnp.ndarra
     tensor_product_res = []
 
     for feat1_idx in range(num_irrep1_feats):
-        for feat2_idx in range(num_irrep2_feats):
+        for parity1 in range(0,2):
+            for parity2 in range(0,2):
+                for feat2_idx in range(num_irrep2_feats):
+                    if irrep1.is_feature_zero(parity1, feat1_idx) or irrep2.is_feature_zero(parity2, feat2_idx):
+                        continue
 
-            # for each of the features in irrep1 and irrep2, calculate the tensor product
-            for l3 in range(output_l):
-                for m3 in range(-l3, l3 + 1):
+                    # for each of the features in irrep1 and irrep2, calculate the tensor product
+                    for l3 in range(output_l):
+                        for m3 in range(-l3, l3 + 1):
 
-                    # calculate the repr for the output l
-                    for l1 in range(max_l1):
-                        for l2 in range(max_l2):
-                            for m1 in range(-l1, l1 + 1):
-                                for m2 in range(-l2, l2 + 1):
-                                    v1 = irrep1.get_coefficient(feat1_idx, l1, m1)
-                                    v2 = irrep2.get_coefficient(feat2_idx, l2, m2)
-                                    tensor_product_res.append(get_clebsch_gordan(l1, l2, l3, m1, m2, m3)*v1*v2)
+                            # calculate the repr for the output l
+                            for l1 in range(max_l1):
+                                for l2 in range(max_l2):
+                                    for m1 in range(-l1, l1 + 1):
+                                        for m2 in range(-l2, l2 + 1):
+                                            v1 = irrep1.get_coefficient(feat1_idx, l1, m1)
+                                            v2 = irrep2.get_coefficient(feat2_idx, l2, m2)
+                                            tensor_product_res.append(get_clebsch_gordan(l1, l2, l3, m1, m2, m3)*v1*v2)
 
 
 # how do you do the tensor product if you have n irreps for one input, and m irreps for the other input?
