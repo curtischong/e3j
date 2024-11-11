fix l calculation

tensor_product.py
@@ -38,7 +38,8 @@ def tensor_product_v1(irrep1: Irrep, irrep2: Irrep) -> jnp.ndarray:
 
                     # for each of the features in irrep1 and irrep2, calculate the tensor product
                     for l3 in range(l3_min, l3_max + 1):
-                        # float normalize = math.sqrt(2 * l3 + 1)
+                        # normalize = math.sqrt(2 * l3 + 1)
+                        normalize = 1
                         for m3 in range(-l3, l3 + 1):
                             coef_idx = Irrep.coef_idx(l3, m3)
 
@@ -51,7 +52,7 @@ def tensor_product_v1(irrep1: Irrep, irrep2: Irrep) -> jnp.ndarray:
                                             v2 = irrep2.get_coefficient(parity2_idx, feat2_idx, l2, m2)
                                             cg = get_clebsch_gordan(l1, l2, l3, m1, m2, m3)
                                             print(f"l1={l1}, l2={l2}, l3={l3}, m1={m1}, m2={m2}, m3={m3}, v1={v1}, v2={v2}, cg={cg} feat_idx={feat3_idx} p1_idx={parity1_idx} p2_idx={parity2_idx} p3_idx={parity3_idx}")
-                                            out = out.at[parity3_idx, coef_idx, feat3_idx].add(cg*v1*v2)
+                                            out = out.at[parity3_idx, coef_idx, feat3_idx].add(cg*v1*v2*normalize)
     return out
 
 