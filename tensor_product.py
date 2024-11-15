760bbf568d9307f4b2cadd1fa0124c3aeb03d180
Curtis Chong <curtischong5@gmail.com>
2024-11-15 13:03:49 -0800

    revert to simpler tensor product

--- tensor_product.py
+++ tensor_product.py
@@ -7,7 +7,7 @@ import jax
 import jax.numpy as jnp
 
 @jax.jit
-def tensor_product_v1(irrep1: Irrep, irrep2: Irrep, max_output_l: int) -> jnp.ndarray:
+def tensor_product_v1(irrep1: Irrep, irrep2: Irrep, max_l3: Optional[int]) -> jnp.ndarray:
     max_l1 = irrep1.l()
     max_l2 = irrep2.l()
 
@@ -16,33 +16,43 @@ def tensor_product_v1(irrep1: Irrep, irrep2: Irrep, max_output_l: int) -> jnp.nd
     num_irrep2_feats = irrep2.num_features()
     num_output_feats = num_irrep1_feats * num_irrep2_feats
 
-    assert max_output_l <= max(max_l1, max_l2) + 1 # TODO: verify that the max_output_l can grow at most 1 more than the largest of the input ls
+    if max_l3 is None:
+        max_output_l = max_l1 + max_l2
+    else:
+        max_output_l = max_l3
 
     num_coefficients_per_feat = (max_output_l+1)**2 # l=0 has 1 coefficient, l=1 has 3, l=2 has 5, etc. This formula gives the sum of all these coefficients
 
     out = jnp.zeros((NUM_PARITY_DIMS, num_coefficients_per_feat, num_output_feats), dtype=jnp.float32)
 
     for feat1_idx in range(num_irrep1_feats):
-        for parity1 in range(0,2):
-            for parity2 in range(0,2):
+        for parity1_idx in PARITY_IDXS:
+            for parity2_idx in PARITY_IDXS:
                 for feat2_idx in range(num_irrep2_feats):
-                    if irrep1.is_feature_zero(parity1, feat1_idx) or irrep2.is_feature_zero(parity2, feat2_idx):
+                    if irrep1.is_feature_zero(parity1_idx, feat1_idx) or irrep2.is_feature_zero(parity2_idx, feat2_idx):
                         continue
 
                     feat3_idx = feat1_idx * num_irrep2_feats + feat2_idx
-                    parity3 = parity1 * parity2
+                    parity3 = parity_idx_to_parity(parity1_idx) * parity_idx_to_parity(parity2_idx)
+                    parity3_idx = parity_to_parity_idx(parity3)
 
-                    # for each of the features in irrep1 and irrep2, calculate the tensor product
-                    for l3 in range(max_output_l):
-                        start_idx_of_l = l3**2
-                        for m3 in range(-l3, l3 + 1):
 
-                            # calculate the repr for the output l
-                            for l1 in range(max_l1):
-                                for l2 in range(max_l2):
-                                    for m1 in range(-l1, l1 + 1):
-                                        for m2 in range(-l2, l2 + 1):
-                                            v1 = irrep1.get_coefficient(parity1, feat1_idx, l1, m1)
-                                            v2 = irrep2.get_coefficient(parity2, feat2_idx, l2, m2)
-                                            out = out.at[parity3, start_idx_of_l + m3, feat3_idx].add(get_clebsch_gordan(l1, l2, l3, m1, m2, m3)*v1*v2)
+                    # calculate the repr for the output l
+                    for l1 in range(max_l1):
+                        for l2 in range(max_l2):
+
+                            # for each of the features in irrep1 and irrep2, calculate the tensor product
+                            l3_min = abs(l1 - l2)
+                            for l3 in range(l3_min, l1 + l2 + 1):
+                                for m1 in range(-l1, l1 + 1):
+                                    for m2 in range(-l2, l2 + 1):
+                                        m3 = m1 + m2
+                                        if m3 < -l3 or m3 > l3:
+                                            continue
+                                        v1 = irrep1.get_coefficient(parity1_idx, feat1_idx, l1, m1)
+                                        v2 = irrep2.get_coefficient(parity2_idx, feat2_idx, l2, m2)
+                                        coef_idx = Irrep.coef_idx(l3, m3)
+                                        cg = get_clebsch_gordan(l1, l2, l3, m1, m2, m3)
+                                        normalization = 1
+                                        out = out.at[parity3_idx, coef_idx, feat3_idx].add(cg*v1*v2*normalization)
     return out
